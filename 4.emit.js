6872f68fe69dac46958f465514fcbef0ec135c83
ksone <tls0401@126.com>
2019-03-30 23:15:08 +0800

    updates

---
+++ 4.emit.js
@@ -0,0 +1,33 @@
+let fs   = require("fs");
+let path = require("path");
+
+// 发布订阅
+// 订阅 [fn, fn]
+// 发布 [].forEach
+function EventEmitter(){
+    this._arr = [];
+}
+EventEmitter.prototype.on = function(callback){
+    this._arr.push(callback);
+}
+EventEmitter.prototype.emit = function(){
+    this._arr.forEach(fn=>fn.apply(this, arguments));
+}
+
+let e = new EventEmitter();
+let school = {};
+e.on((data, key)=>{
+    school[key] = data;
+    if(Object.keys(school).length===2){
+        console.log(school);
+    }
+});
+
+fs.readFile(path.resolve(__dirname, 'name.txt'), 'utf8', (err, data)=>{
+    if(err) return console.log(err);
+    e.emit(data, 'name');
+});
+fs.readFile(path.resolve(__dirname, 'age.txt'), 'utf8', (err, data)=>{
+    if(err) return console.log(err);
+    e.emit(data, 'age');
+});
