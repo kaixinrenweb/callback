c02eee6eae70a888afa5d7f3a81031d2773bc82c
ksone <tls0401@126.com>
2019-03-30 23:02:16 +0800

    add

--- 3.paralle.js
+++ 3.paralle.js
@@ -1,9 +1,30 @@
 // 并发的调用接口
 // 两个ajax ajax1=>name ajax2=>age = name+age
-
-let fs = require("fs");
+let fs   = require("fs");
 let path = require("path");
+
+function after(times, callback){
+    let results= {};
+    return (key, data)=>{
+        results[key] = data;
+        if(--times===0){
+            callback(results);
+        }
+    }
+}
+let newFn = after(2, (results)=>{
+    console.log(results);
+});
+
+
 fs.readFile(path.resolve(__dirname, 'name.txt'), 'utf8', (err, data)=>{
     if(err) return console.log(err);
-    console.log(data);
+    newFn('name', data);
 });
+fs.readFile(path.resolve(__dirname, 'age.txt'), 'utf8', (err, data)=>{
+    if(err) return console.log(err);
+    newFn('age', data);
+});
+
+// 串行 两个人有关系 上一个输出是下一个人的输入
+// 并行 两个人没有关系 可以一起执行
